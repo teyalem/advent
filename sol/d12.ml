98333ff85822347729033d1ed398c7b47e65b39a
Hyeonung Baek <wiserlix@gmail.com>
2020-12-29 02:46:19 +0900

    Day 12, 13, 14

---
+++ sol/d12.ml
@@ -0,0 +1,145 @@
+open Advent
+
+let gravity a b =
+  match compare a b with
+  | 0 -> 0, 0 (* a = b *)
+  | -1 -> +1, -1 (* a < b *)
+  | 1 -> -1, +1 (* a > b *)
+  | _ -> assert false
+
+module Vec3D = struct
+  type t = { x: int;
+             y: int;
+             z: int; }
+
+  let make x y z = { x = x; y = y; z = z }
+
+  let x c = c.x
+  let y c = c.y
+  let z c = c.z
+
+  let map f a = make (f a.x) (f a.y) (f a.z)
+
+  let map2 f a b = { x = f a.x b.x;
+                     y = f a.y b.y;
+                     z = f a.z b.z; }
+
+  let fold f a = f (f a.x a.y) a.z
+
+  let add a b = map2 (+) a b
+
+  let compare a b =
+    match compare a.x b.x with
+    | 0 -> begin
+        match compare a.y b.y with
+        | 0 -> compare a.z b.z
+        | c -> c
+      end
+    | c -> c
+
+end
+
+module Moon = struct
+  type t = { mutable position: Vec3D.t;
+             mutable velocity: Vec3D.t; }
+
+  let parse str =
+    Scanf.sscanf str "<x=%d, y=%d, z=%d>"
+      (fun x y z -> { position = Vec3D.make x y z;
+                      velocity = Vec3D.make 0 0 0; })
+
+  let position m = m.position
+  let velocity m = m.velocity
+
+  let apply_velocity m =
+    m.position <- Vec3D.add m.position m.velocity
+
+  let apply_gravity a b =
+    let ga = Vec3D.map2 (fun a b -> gravity a b |> fst) a.position b.position
+    and gb = Vec3D.map2 (fun a b -> gravity a b |> snd) a.position b.position in
+    a.velocity <- Vec3D.add a.velocity ga;
+    b.velocity <- Vec3D.add b.velocity gb
+
+  let potential_energy m =
+    Vec3D.map abs m.position |> Vec3D.fold (+)
+
+  let kinetic_energy m =
+    Vec3D.map abs m.velocity |> Vec3D.fold (+)
+
+  let total_energy m =
+    potential_energy m * kinetic_energy m
+end
+
+let rec unique_pairs = function
+  | [] | [_] -> assert false
+  | [a; b] -> [a, b]
+  | x::xs -> List.map (fun b -> x, b) xs @ unique_pairs xs
+
+let rec gcd a b =
+  if b = 0
+  then a
+  else gcd b (a mod b)
+
+let lcm a b =
+  let g = gcd a b in
+  abs (a*b) / g
+
+let fold f l = List.(fold_left f (hd l) (tl l))
+
+type pv = { mutable p: int; mutable v: int; }
+
+let copy m = { p = m.p; v = m.v; }
+
+let find_loop first_pl =
+  let rec loop i prev pvl =
+    unique_pairs pvl
+    |> List.iter (fun (m1, m2) ->
+        let g1, g2 = gravity m1.p m2.p in
+        m1.v <- m1.v + g1;
+        m2.v <- m2.v + g2);
+
+    List.iter (fun m -> m.p <- m.p + m.v) pvl;
+    let pl = List.map (fun m -> m.p) pvl in
+    if pl = first_pl && pl = prev
+    then i
+    else loop (i+1) pl pvl
+  in
+
+  let pvl = List.map (fun p -> { p = p; v = 0 }) first_pl in
+  loop 1 first_pl pvl
+
+let main path =
+  let data () = open_in path |> IO.read_lines |> List.map Moon.parse in
+  begin
+    (* PART 1 *)
+    let moons = data () in
+    for _ = 1 to 1000 do
+      unique_pairs moons
+      |> List.iter (fun (a, b) -> Moon.apply_gravity a b);
+      List.iter Moon.apply_velocity moons
+    done;
+    List.fold_left (fun p m -> p + Moon.total_energy m) 0 moons
+    |> print_int;
+    
+    print_newline ();
+
+    (* PART 2 *)
+    let moons = data () in
+    let xpl, ypl, zpl =
+      List.map (fun m -> Moon.position m) moons
+      |> List.map (fun p -> let open Vec3D in x p, y p, z p)
+      |> (fun l ->
+          List.fold_right
+            (fun (x, y, z) (xl, yl, zl) -> x::xl, y::yl, z::zl)
+            l ([], [], []))
+    in
+
+    fold lcm 
+      [ find_loop xpl;
+        find_loop ypl;
+        find_loop zpl; ]
+    |> print_int
+
+  end
+
+let _ = Arg.parse [] main ""
---
+++ sol/d14.ml
@@ -0,0 +1,139 @@
+open Advent
+
+let int_ceiling n base =
+  if n mod base = 0
+  then n
+  else n - n mod base + base
+
+module Reaction = struct
+  type chem = int * string
+
+  let amount (i, _) = i
+  let name (_, chem) = chem
+
+  type t = chem * chem list
+
+  let parse str =
+    let parse_chem str =
+      Scanf.sscanf str " %d %s" (fun n chem -> n, chem)
+    in
+
+    let parse_source_list str =
+      Delim.split "," str
+      |> List.map String.trim
+      |> List.map parse_chem
+    in
+
+    Scanf.sscanf str "%s@=> %d %s" (fun source n chem ->
+        (n, chem), parse_source_list source)
+
+  let print reactions =
+    List.iter (fun (chem, source) ->
+        List.iter (fun (n, c) -> Printf.printf " %d %s," n c) source;
+        Printf.printf "=> %d %s\n" (amount chem) (name chem);)
+      reactions
+
+  let find chem reactions =
+    try List.find (fun (c, _) -> name c = chem) reactions
+    with _ -> failwith chem
+
+  let find_req chem reactions =
+    find chem reactions |> snd
+
+  let find_base chem reactions =
+    find chem reactions |> fst |> fst
+
+  let find_outputs chem reactions =
+    List.filter (fun (_, sources) ->
+        List.exists (fun c -> name c = chem) sources) reactions
+    |> List.map fst
+
+  let level_graph reactions =
+    let rec loop tmp (last: string list) =
+      if List.hd last = "FUEL"
+      then last::tmp
+      else
+        let next = List.concat_map (fun c -> find_outputs c reactions) last
+                   |> List.map (fun c -> name c)
+                   |> List.sort_uniq compare
+        in
+        loop (last::tmp) next
+    in
+    loop [] ["ORE"]
+    |> List.mapi (fun i l -> i, l)
+    |> List.concat_map (fun (i, l) -> List.map (fun n -> n, i) l)
+
+  let level chem levels =
+    List.assoc chem levels
+
+  let min_level chems levels =
+    List.map (fun c -> level (name c) levels) chems |> List.fold_left min max_int
+
+  let group_same_chem chems =
+    List.sort (fun a b -> compare (name a) (name b)) chems
+    |> List.fold_left (fun p (n, c) ->
+        match p with
+          [] -> [n, c]
+        | (i, chem) :: rest ->
+          if c = chem
+          then (n+i, chem) :: rest
+          else (n, c) :: (i, chem) :: rest)
+      []
+
+  let simplify_list chems levels reactions =
+    let open List in
+    let l = min_level chems levels in
+    let ps, ns = partition (fun chem -> level (name chem) levels = l) chems in
+    let ps = ps
+             |> concat_map (fun chem ->
+                 find_req (name chem) reactions
+                 |> map (fun (n, c) ->
+                     let base = find_base (name chem) reactions in
+                         (amount chem)/base*n, c))
+    in
+    let next = group_same_chem (ps @ ns) in
+    let l = min_level next levels in
+    let pps, nns = partition (fun chem -> level (name chem) levels = l) next in
+    let pps = pps
+              |> map (fun (n, chem) ->
+                  if chem = "ORE"
+                  then n, chem
+                  else
+                    let base = find_base chem reactions in
+                    int_ceiling n base, chem)
+    in
+    group_same_chem (pps @ nns)
+
+
+  let calculate_ore chems reactions =
+    let levels = level_graph reactions in
+    let rec loop = function
+        [] -> assert false
+      | [i, "ORE"] -> i
+      | chems -> loop @@ simplify_list chems levels reactions
+    in
+    loop chems
+
+end
+
+let main path =
+  let data = open_in path |> IO.read_lines |> List.map Reaction.parse in
+  begin
+    (* PART 1 *)
+    let one_fuel = Reaction.calculate_ore [1, "FUEL"] data in
+    print_int one_fuel;
+
+    print_newline ();
+
+    (* PART 2 *)
+    (* thanks to u/Diderikdm *)
+    let ore_amount = 1_000_000_000_000 in
+    let lower_bound = ore_amount / one_fuel in
+    let f = Reaction.calculate_ore [lower_bound, "FUEL"] data in
+    let offset = float ore_amount /. float f in
+    let fuels = int_of_float (float lower_bound *. offset) in
+    print_int fuels;
+
+  end
+
+let () = Arg.parse [] main ""
